98ff41c38a42b9cd55e287cad36555f9a6c11d93
shuihsui <1610969069@qq.com>
2018-04-05 06:31:46 +0800

    Add the other code when i learn before

---
+++ python_study/web_program/SocketServer_WEB\.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding:utf8
+
+from SocketServer import (TCPServer as TCP,StreamRequestHandler as SRH)
+from time import ctime
+
+HOST = ''
+PORT =12345
+ADDR = (HOST,PORT)
+
+class MyRequestHandler(SRH):
+	def handle(self):
+		print '...connected from:',self.client_address
+		self.wfile.write('[%s] %s' % (ctime,self.rfile.readline()))
+
+tcpServ = TCP(ADDR,MyRequestHandler)
+print 'waiting for connection...'
+tcpServ.serve_forever()
+
